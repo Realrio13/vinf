17b6a35144e82d3b2cf795b21301568207d07188
Realrio13 <ghu89ghu02@gmail.com>
2022-12-14 15:16:31 +0100

    finalne odovzdanie

--- etymology.py
+++ etymology.py
@@ -2,24 +2,28 @@ import pandas as pd
 from difflib import SequenceMatcher
 import re
 
-index = pd.read_csv("/home/index.csv")
+index = pd.read_csv("index.csv")
 
 def searching(inp):
-    
-    searchRes = index[index['Word']==inp]   # Vyhladavanie slova v indexe
+    """Main function for searching
+
+    :param inp: user inputted word
+    :returns: the etymologies of the word in a string 
+    """
+    searchRes = index[index['Word']==inp]   # Searching for word in index
     if len(searchRes) == 0:
         print("\nWord not found")
         return 0
         
     uniqueLang = []
     for i in range(len(searchRes)):
-        if searchRes["Language"].values[i] not in uniqueLang:
+        if searchRes["Language"].values[i] not in uniqueLang:   # Finding possible anguages for the word
             uniqueLang.append(searchRes["Language"].values[i])
 
     print("\nChoose language by typing in number\n--------------------------")
 
     for i in range(len(uniqueLang)):
-        print(str(i+1) + ") " + uniqueLang[i])    # Listovanie moznych jazykov
+        print(str(i+1) + ") " + uniqueLang[i])    # Listing possible languages
     print("--------------------------")
     
     try:
@@ -29,7 +33,7 @@ def searching(inp):
         return 0 
 
     try:
-        language = uniqueLang[temp-1] # Vyberanie jazyka podla inputu
+        language = uniqueLang[temp-1] # Choosing a language based on input
     except:
         print("Invalid number chosen")
         return 0 
@@ -39,10 +43,11 @@ def searching(inp):
         if searchRes["Language"].values[i] == language:
             findPages.append(searchRes["Locations"].values[i])
 
-    # Prerekvizity hotove, nasleduje search
-    # Z prerekvizit mame slovo, jazyk a strany, kde sa nachadza
+    # Prereqisites done followed by search
+    # Right now we have the word, it's languages and it's locations
     
-    dataFile = open("/home/enwiktionary-20220920-pages-articles-multistream.xml", 'r', encoding="utf8")
+    #  Update for consultation 5, we need to get the correct page from the word's location
+    dataFile = open("data/enwiktionary-20220920-pages-articles-multistream.xml", 'r', encoding="utf8")
     linecount = 0
     page = 0
     inPage = False
@@ -50,28 +55,29 @@ def searching(inp):
     numOfPages = 0
     pageEnd = re.compile("</page>")
     pageStart = re.compile("<page>")
-    for line in dataFile:
+    for line in dataFile:   # Finds line about 3000 lines before entry in index
         if linecount > findPages[numOfPages] - 3000:
             inPage = True
-        if inPage:
-            if pageStart.findall(line):
+        if inPage:  # When within 3000 lines, starts search for page start
+            if pageStart.findall(line): # Found page start
                 temppage = linecount
-            if (searchRes["Word"].values[numOfPages] in line) and (language in line):
+            if (searchRes["Word"].values[numOfPages] in line) and (language in line):   # When the word in found inside the page, line of page is saved
                 findPages[numOfPages] = temppage
                 inPage = False
                 numOfPages += 1
                 if numOfPages == len(findPages):
                     break
         linecount+=1
+    # With the starts of the pages we continue as we were before
 
     findPages = list(dict.fromkeys(findPages))
     numOfPages = 0
-    dataFile = open("/home/enwiktionary-20220920-pages-articles-multistream.xml", 'r', encoding="utf8")
+    dataFile = open("data/enwiktionary-20220920-pages-articles-multistream.xml", 'r', encoding="utf8")
     linecount = 0
     page = 0
     numOfPages = 0
     inPage = False
-    for line in dataFile:
+    for line in dataFile: # We find the begining of the page
         if linecount == findPages[numOfPages]:
             inPage = True
             temp = ""
@@ -79,7 +85,7 @@ def searching(inp):
             if pageEnd.findall(line):
                 inPage = False
                 numOfPages += 1
-                results.append(temp)
+                results.append(temp)    # Append the entire page until the end in results
                 if numOfPages == len(findPages):
                     break
             else:
@@ -88,11 +94,11 @@ def searching(inp):
         linecount+=1
     oglanguages = []
     ogtranslations = []
-    for page in results:
+    for page in results:    # We find the translation of the word and language for each found page
         oglanguages.append(re.search("(?<===)([A-Z][a-z]+)(?===)",page).group())
         ogtranslations.append(re.search("(?<=<title>)(.*)(?=<\/title>)",page).group())
     
-    if len(results) > 1:
+    if len(results) > 1:    # The word can have multiple translations, we ask user, which one they mean
         print("\nDo you mean?\n--------------------------")
         for i in range(len(results)):
             print(str(i+1) + ") " + ogtranslations[i] + "  -  " + oglanguages[i])
@@ -110,38 +116,39 @@ def searching(inp):
         chosen = 0 
         print("Found page: "  + ogtranslations[0] + "  -  " + oglanguages[0])
     print("==========================")
-
+    # We have finally chosen only one word
+    # Time to find etymology
     etymo = []
     i=0
 
     inPage=False
-    if "translations" in ogtranslations[chosen]:
-        print(re.search(".*(?=\/)",ogtranslations[chosen]).group())
+    if "translations" in ogtranslations[chosen]:    # Some pages are just translations, then we have to search for the original page
+        print(re.search(".*(?=\/)",ogtranslations[chosen]).group()) # This doesn't happen often, but it can make search a bit longer
         return searching(re.search(".*(?=\/)",ogtranslations[chosen]).group())
     else:
         for line in results[chosen].splitlines():
-            if line:
+            if line:    # Search for etymologies in the chosen page
                 if inPage:
                     if re.compile("==").findall(line):
                         print("--------------------------")
                         inPage=False
                     else:
                         print(line)
-                else:
+                else: # Found etymologies
                     if re.compile("==[A-Z][a-z]+ ?[A-Z]?[a-z]* ?[A-Z]?[a-z]*==$").findall(line):
-                        temp = ("In language " + re.search("(?<===)[A-Z][a-z]+ ?[A-Z]?[a-z]* ?[A-Z]?[a-z]*(?===)",line).group() + ":")
+                        temp = ("In language " + re.search("(?<===)[A-Z][a-z]+ ?[A-Z]?[a-z]* ?[A-Z]?[a-z]*(?===)",line).group() + ":")  # Language of etymology
                     if re.compile("===Etymology ?[1-9]?===$").findall(line):
                         inPage=True
-                        print(temp)
-        print("==========================\nSearch for simillar words?\n--------------------------\n1: Yes\n2: No\n--------------------------")
-        if input() == '1':
-            treshold = 0.60 # Tu zmenit podobnost slov
+                        print(temp) # Prints all etymologies
+        print("==========================\nSearch for similar words?\n--------------------------\n1: Yes\n2: No\n--------------------------")
+        if input() == '1':  # Bonus functionality, finds similar words
+            treshold = 0.60 # Here change difference treshold
             simwords = set()
             for i in index["Word"]:
                 if len(str(i)) == len(inp):
                     matchMetric = SequenceMatcher(None, i, inp).ratio()
                     if matchMetric > treshold and matchMetric < 1:
-                        simwords.add(i)
+                        simwords.add(i) # Finds similar words in index of the same length
             print("--------------------------")
             temp = 0
             arr = []
@@ -149,7 +156,7 @@ def searching(inp):
                 temp+=1
                 arr.append(i)
                 print(str(temp) + ": " + i)
-                if temp == 10: break
+                if temp == 10: break   # Prints 10 of the found words
             print("--------------------------")
             try:
                 temp = int(input())
@@ -160,13 +167,13 @@ def searching(inp):
                 print("Input is out of range")
                 return 0
             print("Starting search for word " + arr[temp-1] + "...")
-            searching(arr[temp-1])
+            searching(arr[temp-1])  # Searches again for chosen word
             return etymo
         else:
             return etymo
 
 if __name__ == "__main__":
-    print("Enter word...")
+    print("Enter word...")  # Main function, here it all begins
 
     inp = input()
     result = searching(inp)
--- testindex.py
+++ testindex.py
@@ -3,14 +3,23 @@ from pyspark.sql import SparkSession
 import re
 from operator import add
 
-def f(inp):
+#   Indexing program for etymology.py
+#   Output is file index.csv, which is needed for etymology.py
+#   Required data: enwictionary
+
+def f(inp): # Finds translations of words and returns them
+    """Mapper for spark
+
+    :param inp: one line in enwictionary
+    :returns: all translations in that line
+    """
     linecount,line = inp
-    data = []
-    if dictionaryPattern.findall(line):
+    data = []   # array, because one word can have multiple translations in the same language
+    if dictionaryPattern.findall(line): # identifies only lines with translations
         language = re.search("[A-Z][a-z]+ ?[A-Z]?[a-z]*",line).group()
-        titles = re.findall("(?<=\|[a-z]{2}\|)[^{},|]+(?=[\|}])",line, re.DOTALL)
+        titles = re.findall("(?<=\|[a-z]{2}\|)[^{},|]+(?=[\|}])",line, re.DOTALL)   # saves data
         for title in titles:
-            data.append((title, language, str(linecount-1)))
+            data.append((title, language, str(linecount-1)))    # appends data to array
     return data
 
 inPage = 0
@@ -22,7 +31,7 @@ anyTitle = re.compile("<title>")
 ogLanguage = re.compile("===[^=]+===")
 capitalLetter = re.compile("[A-Z]")
 dictionaryPattern = re.compile("^[*]:? [A-Z][a-z]+ ?[A-Z]?[a-z]*: {{")
-transPattern = re.compile("(?<=\|[a-z]{2}\|)([^{},|]*)(?=(\|[a-z])|(}}))")
+transPattern = re.compile("(?<=\|[a-z]{2}\|)([^{},|]*)(?=(\|[a-z])|(}}))")  # regexes to search in the data
 
 datafile = open("/home/enwiktionary-20220920-pages-articles-multistream.xml", 'r')
 #enwiktionary-20220920-pages-articles-multistream
@@ -31,28 +40,27 @@ spark = SparkSession\
         .appName("PythonPi")\
         .getOrCreate()
 
-data = spark.sparkContext.parallelize(enumerate(datafile), 10).map(f).reduce(add)
+data = spark.sparkContext.parallelize(enumerate(datafile), 10).map(f).reduce(add) # spark function
 
 inPage = 0
 page = 0
 datafile = open("/home/enwiktionary-20220920-pages-articles-multistream.xml", 'r')
-print("halo")
-for line in datafile:
-    if inPage and page > 4:
+for line in datafile:   # finds titles of pages (they can also be translations)
+    if inPage and page > 4: # starts from page 4
         if pageEnd.findall(line): inPage = 0
-        elif anyTitle.findall(line) and not capitalLetter.findall(line): 
+        elif anyTitle.findall(line) and not capitalLetter.findall(line): # looks only for those titles that don't start with: Translations:word
             titlefound = 1
             saveline = linecount - 1
-            title = re.search("(?<=<title>)(.*)(?=<\/title>)",line).group()
-        elif titlefound and ogLanguage.findall(line):
+            title = re.search("(?<=<title>)(.*)(?=<\/title>)",line).group() # finds word from title using regex
+        elif titlefound and ogLanguage.findall(line):   # immediatly after word we need to find the language
             titlefound = 0
             if title:
-                language = re.search("(?<===)(.*)(?===)",line).group()
+                language = re.search("(?<===)(.*)(?===)",line).group() # getting language for the title word
                 if '=' not in language: 
-                    data.append((title, re.search("(?<===)(.*)(?===)",line).group(), str(saveline)))
+                    data.append((title, re.search("(?<===)(.*)(?===)",line).group(), str(saveline)))  # saving data and languages
     if pageStart.findall(line): inPage = 1
 
 df = pd.DataFrame(data, columns=['Word', 'Language', 'Locations'])
-df.to_csv("/home/index.csv", index=False)
+df.to_csv("/home/index.csv", index=False)   # saving to index.csv
 
 
